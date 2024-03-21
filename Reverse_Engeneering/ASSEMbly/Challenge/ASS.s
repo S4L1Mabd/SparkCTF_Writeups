6a46143d60b4fdf9f7f47f0582780273db248175
Salim ABDOUNE <114289045+ABDOUNEsalim@users.noreply.github.com>
2024-03-21 23:07:44 +0100

    Add files via upload

---
+++ Reverse_Engeneering/ASSEMbly/Challenge/ASS.s
@@ -0,0 +1,64 @@
+	.file	"ASS.c"
+	.intel_syntax noprefix
+	.text
+	.globl	func
+	.type	func, @function
+func:
+.LFB0:
+	.cfi_startproc
+	push	rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	mov	rbp, rsp
+	.cfi_def_cfa_register 6
+	mov	DWORD PTR -20[rbp], edi
+	mov	eax, DWORD PTR -20[rbp]
+	cdqe
+	sal	rax, 3
+	mov	QWORD PTR -8[rbp], rax
+	xor	QWORD PTR -8[rbp], 5
+	movabs	rax, 5934314573
+	cmp	QWORD PTR -8[rbp], rax
+	sete	al
+	movzx	eax, al
+	pop	rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	func, .-func
+	.section	.rodata
+.LC0:
+	.string	"Output: %d\n"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB1:
+	.cfi_startproc
+	push	rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	mov	rbp, rsp
+	.cfi_def_cfa_register 6
+	sub	rsp, 16
+	mov	DWORD PTR -4[rbp], "XXXXXXXXX"
+	mov	eax, DWORD PTR -4[rbp]
+	mov	edi, eax
+	call	func
+	mov	DWORD PTR -8[rbp], eax
+	mov	eax, DWORD PTR -8[rbp]
+	mov	esi, eax
+	lea	rax, .LC0[rip]
+	mov	rdi, rax
+	mov	eax, 0
+	call	printf@PLT
+	mov	eax, 0
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1:
+	.size	main, .-main
+	.ident	"GCC: (Debian 12.2.0-14) 12.2.0"
+	.section	.note.GNU-stack,"",@progbits
